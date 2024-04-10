22a29403361d86350608b4387085b43967c12010
unknown <jpsim1234@naver.com>
2024-04-10 14:11:32 +0900

    commit complete

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,4 +1,5 @@
 package hello.hellospring;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import jakarta.persistence.EntityManager;
@@ -8,22 +9,22 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-
-    private EntityManager em;
+    private final MemberRepository memberRepository;
+//    private EntityManager em;
 
     @Autowired
-    public SpringConfig(EntityManager em){
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-          return new JpaMemberRepository(em);
-    }
+//          return new JpaMemberRepository(em);
+//    }
 }
